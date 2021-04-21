bada5aaf33357192b70ce21d6bbe7fc26153fd2d
Alexis Ligon <aligon17@gmail.com>
2021-04-21 11:36:29 -0400

    questions are objects in js, display question on click

--- script.js
+++ script.js
@@ -21,3 +21,90 @@
 // if a wrong selection is made, ten seconds are deducted from the time 
 //when the quiz is over, display ALL DONE and final score as time left and form for initials and high score
 //submit initials and high score to storage to display
+
+var timer = 0;
+var startBtn = document.querySelector("#starter");
+var submitBtn = document.querySelector("#submit");
+var tryAgainBtn = document.querySelector("#try-again");
+var clearBtn = document.querySelector("#clear");
+var answers = document.querySelectorAll("li");
+var section = document.querySelector(".questions");
+var intro = document.querySelector("#intro");
+var count = 0;
+var questions = [{
+    questionNumber: "Question #1",
+    questionTitle: "Commonly used Data Types do not include:",
+    firstAnswer: "Strings",
+    secondAnswer: "Booleans",
+    thirdAnswer: "Alerts",
+    fourthAnswer: "Numbers",
+    correctAnswer: "Alerts",
+},
+
+{
+    questionNumber: "Question #2",
+    questionTitle: "The condition in an if/else statement is enclosed within: _______.",
+    firstAnswer: "Quotes",
+    secondAnswer: "Curly Brackets",
+    thirdAnswer: "Parenthesis",
+    fourthAnswer: "Square Brackets",
+    correctAnswer: "Parenthesis",
+},
+{
+    questionNumber: "Question #3",
+    questionTitle: "Arrays in Javascript can be used to store _______.",
+    firstAnswer: "Numbers and Strings",
+    secondAnswer: "Other arrays",
+    thirdAnswer: "Booleans",
+    fourthAnswer: "All the above",
+    correctAnswer: "All the above",
+},
+{
+    questionNumber: "Question #4",
+    questionTitle: "String values must be enclosed within _______ when being assigned to variables.",
+    firstAnswer: "Commas",
+    secondAnswer: "Curly brackets",
+    thirdAnswer: "Quotes",
+    fourthAnswer: "Parenthesis",
+    correctAnswer: "Quotes",
+},
+{
+    questionNumber: "Question #5",
+    questionTitle: "A very useful tool used during development and debugging for printing content to the debugger is:",
+    firstAnswer: "JavaScript",
+    secondAnswer: "Terminal/bash",
+    thirdAnswer: "For Loops",
+    fourthAnswer: "Console.log",
+    correctAnswer: "Console.log",
+}]
+
+startBtn.addEventListener("click", function () {
+    //hide intro page
+    intro.style.display = "none";
+    //update values in sections
+    displayQuestions ();
+}
+)
+
+
+function displayQuestions() {
+    var questionNumber = document.querySelector(".questionNumber");
+    var questionTitle = document.querySelector(".questionTitle");
+    var firstAnswer = document.querySelector(".firstAnswer");
+    var secondAnswer = document.querySelector(".secondAnswer");
+    var thirdAnswer = document.querySelector(".thirdAnswer");
+    var fourthAnswer = document.querySelector(".fourthAnswer");
+    var correctAnswer = questions[count].correctAnswer;
+
+    questionNumber.textContent = questions[count].questionNumber;
+    questionTitle.textContent = questions[count].questionTitle;
+
+}
+
+
+
+
+
+
+
+
