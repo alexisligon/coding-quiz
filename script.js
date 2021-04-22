a9da4e31132171e98ddccd93cf6b1172d65bdb8c
Alexis Ligon <aligon17@gmail.com>
2021-04-22 16:02:15 -0400

    added event listener for choice buttons to display next question

--- script.js
+++ script.js
@@ -27,54 +27,65 @@ var startBtn = document.querySelector("#starter");
 var submitBtn = document.querySelector("#submit");
 var tryAgainBtn = document.querySelector("#try-again");
 var clearBtn = document.querySelector("#clear");
-var section = document.querySelector(".questions");
-var intro = document.querySelector("#intro");
-var count = 0;
+var section = document.querySelector(".questions"); //section on html where questions block is displayed
+var intro = document.querySelector("#intro"); //intro section
+var count = 0; //declares array to start at 0;
+var questionNumber = document.querySelector(".questionNumber");//section for question number
+var questionTitle = document.querySelector(".questionTitle");//section for actual question
+var firstAnswer = document.querySelector("#firstChoice");//section for answer selections
+var secondAnswer = document.querySelector("#secondChoice");
+var thirdAnswer = document.querySelector("#thirdChoice");
+var fourthAnswer = document.querySelector("#fourthChoice");
+var correctAnswer;
+var message = document.querySelector("#message");
+var allBtn = document.querySelectorAll(".choices");
+
+//questions as objects in an array
 var questions = [{
     questionNumber: "Question #1",
     questionTitle: "Commonly used Data Types do not include:",
-    firstAnswer: "Strings",
-    secondAnswer: "Booleans",
-    thirdAnswer: "Alerts",
-    fourthAnswer: "Numbers",
-    correctAnswer: "Alerts",
+    firstAnswer: "1.Strings",
+    secondAnswer: "2.Booleans",
+    thirdAnswer: "3.Alerts",
+    fourthAnswer: "4.Numbers",
+    correctAnswer: "3.Alerts",
 },
 
 {
     questionNumber: "Question #2",
     questionTitle: "The condition in an if/else statement is enclosed within: _______.",
-    firstAnswer: "Quotes",
-    secondAnswer: "Curly Brackets",
-    thirdAnswer: "Parenthesis",
-    fourthAnswer: "Square Brackets",
-    correctAnswer: "Parenthesis",
+    firstAnswer: "1.Quotes",
+    secondAnswer: "2.Curly Brackets",
+    thirdAnswer: "3.Parenthesis",
+    fourthAnswer: "4.Square Brackets",
+    correctAnswer: "3.Parenthesis",
 },
 {
     questionNumber: "Question #3",
     questionTitle: "Arrays in Javascript can be used to store _______.",
-    firstAnswer: "Numbers and Strings",
-    secondAnswer: "Other arrays",
-    thirdAnswer: "Booleans",
-    fourthAnswer: "All the above",
-    correctAnswer: "All the above",
+    firstAnswer: "1.Numbers and Strings",
+    secondAnswer: "2.Other arrays",
+    thirdAnswer: "3.Booleans",
+    fourthAnswer: "4.All the above",
+    correctAnswer: "4.All the above",
 },
 {
     questionNumber: "Question #4",
     questionTitle: "String values must be enclosed within _______ when being assigned to variables.",
-    firstAnswer: "Commas",
-    secondAnswer: "Curly brackets",
-    thirdAnswer: "Quotes",
-    fourthAnswer: "Parenthesis",
-    correctAnswer: "Quotes",
+    firstAnswer: "1.Commas",
+    secondAnswer: "2.Curly brackets",
+    thirdAnswer: "3.Quotes",
+    fourthAnswer: "4.Parenthesis",
+    correctAnswer: "3.Quotes",
 },
 {
     questionNumber: "Question #5",
     questionTitle: "A very useful tool used during development and debugging for printing content to the debugger is:",
-    firstAnswer: "JavaScript",
-    secondAnswer: "Terminal/bash",
-    thirdAnswer: "For Loops",
-    fourthAnswer: "Console.log",
-    correctAnswer: "Console.log",
+    firstAnswer: "1.JavaScript",
+    secondAnswer: "2.Terminal/bash",
+    thirdAnswer: "3.For Loops",
+    fourthAnswer: "4.Console.log",
+    correctAnswer: "4.Console.log",
 }]
 
 startBtn.addEventListener("click", function () {
@@ -85,27 +96,30 @@ startBtn.addEventListener("click", function () {
 }
 )
 
+//function to display next question
 function displayQuestions() {
-    var questionNumber = document.querySelector(".questionNumber");
-    var questionTitle = document.querySelector(".questionTitle");
-    var firstAnswer = document.querySelector(".firstAnswer");
-    var secondAnswer = document.querySelector(".secondAnswer");
-    var thirdAnswer = document.querySelector(".thirdAnswer");
-    var fourthAnswer = document.querySelector(".fourthAnswer");
-    var correctAnswer = questions[count].correctAnswer;
-
+    
     questionNumber.textContent = questions[count].questionNumber;
     questionTitle.textContent = questions[count].questionTitle;
     firstAnswer.textContent = questions[count].firstAnswer;
     secondAnswer.textContent = questions[count].secondAnswer;
     thirdAnswer.textContent = questions[count].thirdAnswer;
     fourthAnswer.textContent = questions[count].fourthAnswer;
+    
+};
+
+//variable for all answer buttons
+var firstBtn = document.querySelector("#firstChoice");
+var secondBtn = document.querySelector("#secondChoice");
+var thirdBtn = document.querySelector("#thirdChoice");
+var fourthBtn = document.querySelector("#fourthChoice");
+
+//event listener for all buttons to proceed to next question
+var i = 0, length = allBtn.length
+for (i; i < length; i++){
+allBtn[i].addEventListener("click", function() {
+
+    count++;
+    displayQuestions();
+})
 }
-
-
-
-
-
-
-
-
