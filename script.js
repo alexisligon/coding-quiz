493a141dd14a09293b93840e519c54ccc79bc03e
Alexis Ligon <aligon17@gmail.com>
2021-04-25 13:14:15 -0400

    deleted pseudocode

--- script.js
+++ script.js
@@ -1,27 +1,3 @@
-//============PSEUDOCODE=========//
-
-//AS A coding boot camp student
-// I WANT to take a timed quiz on JavaScript fundamentals that stores high scores
-// SO THAT I can gauge my progress compared to my peers
-
-// GIVEN I am taking a code quiz
-// WHEN I click the start button
-// THEN a timer starts and I am presented with a question
-// WHEN I answer a question
-// THEN I am presented with another question
-// WHEN I answer a question incorrectly
-// THEN time is subtracted from the clock
-// WHEN all questions are answered or the timer reaches 0
-// THEN the game is over
-// WHEN the game is over
-// THEN I can save my initials and my score
-
-//when the start button is pressed, the timer begins counting down and the first question appears
-//when a selection is made, the next question appears, a small message displays whether your selection was correct or wrong
-// if a wrong selection is made, ten seconds are deducted from the time 
-//when the quiz is over, display ALL DONE and final score as time left and form for initials and high score
-//submit initials and high score to storage to display
-
 var startBtn = document.querySelector("#starter");
 var submitBtn = document.querySelector("#submit");
 var tryAgainBtn = document.querySelector("#try-again");
